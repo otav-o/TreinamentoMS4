c272f8c562c4e6f64c7f55456bfd9b15230d9102
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-27 18:28:11 -0300

    Exibição de dois cartões no aplicativo

--- React/olamundo/src/components/Cartao.js
+++ React/olamundo/src/components/Cartao.js
@@ -2,10 +2,26 @@ import React from 'react'; // todo componente do React precisa dar esse import
 
 const Cartao = () => {
     return (
-        <div>
-            <div><strong>Cartão</strong></div>
-            <p>Testando...</p>
-        </div>
+<div className="ui card">
+  <div className="image">
+    <img src="https://semantic-ui.com//images/avatar2/large/kristy.png"/>
+  </div>
+  <div className="content">
+    <a className="header">Kristy</a>
+    <div className="meta">
+      <span className="date">Joined in 2013</span>
+    </div>
+    <div className="description">
+      Kristy is an art director living in New York.
+    </div>
+  </div>
+  <div className="extra content">
+    <a>
+      <i className="user icon"></i>
+      22 Friends
+    </a>
+  </div>
+</div>
     );
 };
 
