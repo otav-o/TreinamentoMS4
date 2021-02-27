48eb5e9adb471a09c5ab656723742bcb94e1ab61
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-27 18:57:22 -0300

    Usando faker API para os dados dos cartões

--- React/olamundo/src/components/Cartao.js
+++ React/olamundo/src/components/Cartao.js
@@ -1,27 +1,28 @@
 import React from 'react'; // todo componente do React precisa dar esse import
+import faker from 'faker';
 
 const Cartao = () => {
     return (
-<div className="ui card">
-  <div className="image">
-    <img src="https://semantic-ui.com//images/avatar2/large/kristy.png"/>
-  </div>
-  <div className="content">
-    <a className="header">Kristy</a>
-    <div className="meta">
-      <span className="date">Joined in 2013</span>
-    </div>
-    <div className="description">
-      Kristy is an art director living in New York.
-    </div>
-  </div>
-  <div className="extra content">
-    <a>
-      <i className="user icon"></i>
-      22 Friends
-    </a>
-  </div>
-</div>
+      <div className="ui card">
+        <div className="image">
+          <img src={faker.image.people()}/>
+        </div>
+        <div className="content">
+          <a className="header">{faker.name.firstName()}</a>
+          <div className="meta">
+            <span className="date">Joined in {faker.date.past().toDateString()}</span>
+          </div>
+          <div className="description">
+          {faker.lorem.sentence()}
+          </div>
+        </div>
+        <div className="extra content">
+          <a>
+            <i className="user icon"></i>
+            {faker.random.number()}
+          </a>
+        </div>
+      </div>
     );
 };
 
