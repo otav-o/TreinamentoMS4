82a94cf1a0c2abb7503fa13ffff7d9289b0b227f
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-05 15:14:45 -0300

    Criação de um vetor de informações e iteração pela função map() para gerar os cartões

--- React/olamundo/src/components/App.js
+++ React/olamundo/src/components/App.js
@@ -5,22 +5,44 @@ import Mensagem from './Mensagem';
 const App = () => {
     return (
         <div>
-            <Cartao 
-                foto='https://avatars.githubusercontent.com/u/60331508?s=400&u=5e3e24f7a44c259c4ea48dfa040fd7e314f9bc6e&v=4' 
-                nome='Otávio' 
-                membroDesde='2021' 
-                descricao='Frase qualquer' 
-                numeroAmigos='10'
-            />
-            <Cartao 
-                foto='https://upload.wikimedia.org/wikipedia/commons/8/82/Pronunciamento_do_Presidente_da_Rep%C3%BAblica%2C_Jair_Bolsonaro_%28cropped%29.jpg' 
-                nome='Jair Messias' 
-                membroDesde='2010' 
-                descricao='Frase qualquer' 
-                numeroAmigos='150'
-            />
+            { // chaves para digitar código js
+                dados.map(x => { // tudo depois do arrow e entre chaves é retornado. Map para percorrer o vetor
+                    return <Cartao 
+                        foto={x.foto}
+                        nome={x.nome}
+                        membroDesde={x.membroDesde}
+                        descricao={x.descricao}
+                        numeroAmigos={x.numeroAmigos}
+                    />
+                })
+            }
+           
         </div>
     )
 };
+
+const dados = [
+    {
+        foto:'https://avatars.githubusercontent.com/u/60331508?s=400&u=5e3e24f7a44c259c4ea48dfa040fd7e314f9bc6e&v=4', 
+        nome:'Otávio' ,
+        membroDesde:'2021' ,
+        descricao:'Frase qualquer', 
+        numeroAmigos:'10'
+    },
+    {
+        foto:'https://upload.wikimedia.org/wikipedia/commons/8/82/Pronunciamento_do_Presidente_da_Rep%C3%BAblica%2C_Jair_Bolsonaro_%28cropped%29.jpg', 
+        nome:'Jair Messias' ,
+        membroDesde:'2010' ,
+        descricao:'Frase qualquer' ,
+        numeroAmigos:'150'
+    },
+    {
+        foto:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoywzbgRHSNs9E9i_UCNWE6HMAFm_g7pL0ZA&usqp=CAU', 
+        nome:'Dilma Rousseff' ,
+        membroDesde:'2007' ,
+        descricao:'Frase qualquer' ,
+        numeroAmigos:'0'
+    }
+]
 
 export default App;
