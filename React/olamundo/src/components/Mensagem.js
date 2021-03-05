d8f06d33cca10a202cd0a345c55d07cda8eea997
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-05 15:00:46 -0300

    Fim do componente Mensagem

--- React/olamundo/src/components/Mensagem.js
+++ React/olamundo/src/components/Mensagem.js
@@ -3,6 +3,6 @@ import React from 'react'
 const Mensagem = (props) => {
     return <div>Olá {props.nome}! {props.mensagem}!</div>;
     // return <div>{`Olá ${props.nome}! Seja bem vindo!`}</div>; // interpolação em JS (também é possível)
-}git a
+}
 
 export default Mensagem;
