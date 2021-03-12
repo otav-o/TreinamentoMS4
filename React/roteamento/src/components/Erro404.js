bf82ceb6c890647c74f62336beb15f17767a8f9a
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-12 11:38:35 -0300

    Criação dos componentes Sobre e Erro404

---
+++ React/roteamento/src/components/Erro404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Erro404 extends React.Component {
+    render () {
+        return (<h1>A página que você está procurando não existe.</h1>);
+    }
+}
+
+export default App;
---
+++ React/roteamento/src/components/Sobre.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Sobre extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Sobre</h1>
+                <p>1, 2, 3, testando...</p>
+            </div>
+        );
+    }
+}
+
+export default App;
