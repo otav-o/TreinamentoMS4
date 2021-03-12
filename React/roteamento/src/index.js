7dcbf023de92a4d239bb18ee86ad4a64d580c235
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-12 11:50:42 -0300

    Criação de Route com os componentes

--- React/roteamento/src/index.js
+++ React/roteamento/src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Sobre from './components/Sobre';
+import Erro404 from './components/Erro404';
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+import {BrowserRouter, Switch, Route} from 'react-router-dom' // npm install react-router-dom
+// BrowserRouter faz o roteamento, Switch recebe vários componentes do tipo Route e renderiza de acordo com a url
+// na hora de renderizar, não usar o próprio componente, e sim o BrowserRouter
+
+ReactDOM.render((
+    <BrowserRouter>
+        <Switch>
+            <Route path='/' exact={true} component={App}/>
+            {/* se o caminho for exatamente /, renderizar o App */}
+            <Route path='/sobre' exact={true} component={Sobre}/>
+            <Route path='/*' component={Erro404}/>
+            {/* qualquer outro path */}
+        </Switch>
+
+    </BrowserRouter>
+    
+    ), document.querySelector('#root'))
