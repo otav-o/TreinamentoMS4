dbc1ee5d3576d7045f2e7ae0d65bc7b76cd4c659
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-13 17:32:39 -0300

    Criação do componente ContatoLista

--- Crud/crud/src/components/App.js
+++ Crud/crud/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContatoLista from './contatos/ContatoLista'
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Lista de contatos</h1>
+            <ContatoLista/>
         );
     }
 }
---
+++ Crud/crud/src/components/contatos/ContatoLista.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContatoLista extends React.Component {
+    render() {
+        return (
+            <h1>Componente contato lista</h1>
+        )
+    }
+}
+
+export default ContatoLista;
+
+// representa uma lista de contatos
