90978aeafbb4444eaaebc9f4da8afcb95f4d26dc
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-13 17:25:52 -0300

    Criação do componente App e uso no index.js

---
+++ Crud/crud/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Lista de contatos</h1>
+        );
+    }
+}
+
+export default App;
