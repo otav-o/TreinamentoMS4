495a78e93a1334e19e4100c43d1cae23d21192c3
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-15 14:40:49 -0300

    Primeira versão do componente

---
+++ Crud/crud/src/components/contatos/ContatoAlterar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ContatoAlterar extends React.Component {
+    render () {
+        const obj = this.props.objeto;
+
+        salvar = (e) => { console.log(e); }
+        return (
+            <div>
+                <button onClick={()=>{this.props.voltar()}} className='tiny ui grey button'>Voltar</button>
+                <form className="ui form">
+                    <div>
+                        <div>
+                            <label>Nome</label>
+                            <input defaultValue={obj.Nome} type="text"></input>
+                        </div>
+                        <div>
+                            <label>Número</label>
+                            <input defaultValue={this.props.objeto.Numero} type="text"/>
+                        </div>
+                    </div>
+                    <button onClick={()=>{this.salvar()}} className='tiny ui green button'>Salvar Alterações</button>
+                </form>
+            </div>
+            );
+    }
+}
+
+export default ContatoConsulta;
