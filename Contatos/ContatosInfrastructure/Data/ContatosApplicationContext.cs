fd4a8582cf87c9c32c11b41e086cb62ce7b616dc
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-19 09:37:38 -0300

    Projeto ContatosInfrastructure para DbContext e Migrations

---
+++ Contatos/ContatosInfrastructure/Data/ContatosApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContatosDomain.Models;
+
+namespace ContatosInfrastructure.Data
+{
+    public class ContatosApplicationContext : DbContext
+    {
+        public ContatosApplicationContext (DbContextOptions<ContatosApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contato>()
+                .HasMany(e => e.Numeros) // entidade contato tem vários números
+                .WithOne()
+                .HasForeignKey(p => p.ContatoId)
+                .OnDelete(DeleteBehavior.Cascade); // deletar em cascata (ao deletar o contato, deletar seus números)
+        }
+
+        public DbSet<Contato> Contato { get; set; }
+    }
+}
---
+++ Contatos/ContatosInfrastructure/Migrations/20210318165815_Versao inicial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContatosInfrastructure.Migrations
+{
+    public partial class Versaoinicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contato",
+                columns: table => new
+                {
+                    ContatoId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contato", x => x.ContatoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContatoNumero",
+                columns: table => new
+                {
+                    ContatoNumeroId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ContatoId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContatoNumero", x => x.ContatoNumeroId);
+                    table.ForeignKey(
+                        name: "FK_ContatoNumero_Contato_ContatoId",
+                        column: x => x.ContatoId,
+                        principalTable: "Contato",
+                        principalColumn: "ContatoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContatoNumero_ContatoId",
+                table: "ContatoNumero",
+                column: "ContatoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContatoNumero");
+
+            migrationBuilder.DropTable(
+                name: "Contato");
+        }
+    }
+}
